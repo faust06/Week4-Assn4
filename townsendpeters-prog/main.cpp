53ff4b9289b2d89739b42d4bd7757631a907f5a1
nrtownsend <nrtownsend@The-Unibody-Pro-2.local>
2015-03-22 14:36:11 -0700

    Blank program, first commit for program

--- townsendpeters-prog/main.cpp
+++ townsendpeters-prog/main.cpp
@@ -1,10 +1,6 @@
-//
-//  main.cpp
-//  townsendpeters-prog
-//
-//  Created by nrtownsend on 3/22/15.
-//  Copyright (c) 2015 nrtownsend. All rights reserved.
-//
+//program heading goes here
+
+
 
 #include <iostream>
 
