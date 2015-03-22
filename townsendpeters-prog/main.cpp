fb51f1255205825398573f36aec757369d710c7b
nrtownsend <nrtownsend@The-Unibody-Pro-2.local>
2015-03-22 14:35:21 -0700

    Initial Commit

---
+++ townsendpeters-prog/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  townsendpeters-prog
+//
+//  Created by nrtownsend on 3/22/15.
+//  Copyright (c) 2015 nrtownsend. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
