c85615fee6e35865bda870158045c679671fe245
nrtownsend <nrtownsend@The-Unibody-Pro-2.local>
2015-03-23 21:24:35 -0700

    Added constants for hash table size and random array values. Also added struct definition for separate chaining hashtable

--- townsendpeters-prog/Common.h
+++ townsendpeters-prog/Common.h
@@ -12,5 +12,18 @@
 #include "HashADT.h"
 #include "OtherFunctions.h"
 
+//constants
+const int   MINIMUM_HASH_TABLE_SIZE = 6700,                     //smallest hashtable size that can be used
+            UNIQUE_VALUES = 5000,                               //number of unique values in array containing random values
+            RANDOM_ARRAY_SEARCH_DIVISON = 2,                    //amount of sections that random array will be divided into for searching purposes
+            MIN_RANDOM_VALUE = 1,                               //smallest random value that can be placed into array
+            MAX_RANDOM_VALUE = 30000;                           //largest random value that can be placed into array
+
+//structs
+struct hashNode {
+    int value;
+    hashNode *next;
+};
+
 
 #endif
