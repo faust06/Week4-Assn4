19d9261bd6cbdccb1d45601fe82471a775c8f0f4
nrtownsend <nrtownsend@The-Unibody-Pro-2.local>
2015-03-23 23:02:33 -0700

    Changed constant RANDOM_ARRAY_UNIQUE_VALUES back to 5000.

--- townsendpeters-prog/Common.h
+++ townsendpeters-prog/Common.h
@@ -15,7 +15,7 @@
 
 //constants
 const int   HASH_TABLE_MINIMUM_SIZE = 6700,                     //smallest hashtable size that can be used
-            RANDOM_ARRAY_UNIQUE_VALUES = 250,                  //number of unique values in array containing random values
+            RANDOM_ARRAY_UNIQUE_VALUES = 5000,                  //number of unique values in array containing random values
             RANDOM_ARRAY_SEARCH_DIVISION = 2,                   //amount of sections that random array will be divided into for searching purposes
             RANDOM_ARRAY_VALUE_MIN = 1,                         //smallest random value that can be placed into array
             RANDOM_ARRAY_VALUE_MAX = 30000,                     //largest random value that can be placed into array
