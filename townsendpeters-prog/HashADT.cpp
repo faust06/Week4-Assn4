6772abae92db148d03f9a739ee54652c85acedb0
Chad <cpeters008@gmail.com>
2015-03-25 01:38:02 +0100

    Update HashADT.cpp
    1. InitializeOpenTbl - initializes array and index array cells all to 0. Ref. pg. 519-520 in our book for why the index array is nice. I added it so that the searching / finding data in hash table will be better managed / easier.
    2. InitializeChnTbl - initializes array of pointers to NULL

--- townsendpeters-prog/HashADT.cpp
+++ townsendpeters-prog/HashADT.cpp
@@ -23,3 +23,75 @@ double CalculateLoadFactor(int hashTableSize)
     //returns calculated load factor
     return loadFactor;
 }
+
+
+//*********************************************************************
+// FUNCTION: 		InitializeOpenTbl()
+// DESCRIPTION: 	Initializes hash table as array of integers for open addressing
+// INPUT:
+// 	Parameters: 	openHashTbl - hash table array of integers for open addressing
+//
+//					idxStatusList - array the same size as hash table indicating
+//					that the corresponding position in the hasht able is empty
+//					(0, empty, 1, full, -1, removed)
+//
+//					hashTableSize - size of hash table from user
+//
+// OUTPUT:
+// 	Return Val: 	openHashTbl - initialized array of ints for hash table
+//					passed back by reference
+//
+//					idxStatusList - initialized array of corresponding ints
+//					as indicators for hash table (0, empty, 1, full, -1, removed)
+//					passed back by reference
+//
+// IMPLEMENTED BY: 	Chad Peters
+//**********************************************************************
+
+void InitializeOpenTbl(int* &openHashTbl, int* &idxStatusList, int hashTableSize)
+{
+
+	// allocate memory for arrays
+	openHashTbl = new (std::nothrow) int[hashTableSize];
+	idxStatusList = new (std::nothrow) int[hashTableSize];
+
+	// initialize all cells in openHashTable and idxStatusList to 0	
+	for(int i = 0; i < hashTableSize; i++){
+	
+		openHashTbl[i] = 0;
+		idxStatusList[i] = 0;
+		
+	} // end for
+	
+}
+
+//*********************************************************************
+// FUNCTION: 		InitializeChnTbl()
+// DESCRIPTION: 	Initializes hash table as array of pointers
+// INPUT:
+// 	Parameters: 	chnHashTbl - hash table array of pointers for chained hashing
+//
+//					hashTableSize - size of hash table from user
+//
+// OUTPUT:
+// 	Return Val: 	chnHashTbl - initialized array of ints for hash table
+//					passed back by reference
+//
+//
+// IMPLEMENTED BY: 	Chad Peters
+//**********************************************************************
+
+chnArray *InitializeChnTbl(chnArray* &chnHashTbl, int hashTableSize)
+{
+
+	// allocate memory for array OF POINTERS
+	chnHashTbl = new (std::nothrow) chnArray[hashTableSize];
+
+	// initialize all cells in openHashTable and idxStatusList to 0	
+	for(int i = 0; i < hashTableSize; i++){
+	
+		chnHashTbl[i].link = NULL;
+		
+	} // end for
+	
+}
