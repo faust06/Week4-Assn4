52d6f5177116f7f6e356daf116a14c6273719483
Chad <cpeters008@gmail.com>
2015-03-27 05:50:24 +0100

    Update OtherFunctions.h
    Added new functions as headers

--- townsendpeters-prog/OtherFunctions.h
+++ townsendpeters-prog/OtherFunctions.h
@@ -8,9 +8,11 @@
 
 double CalculateAverage(int );
 double CalculateKnuthAverage(int , int , double );
+int RandomNum();
 int GetMenuChoice();
 int GetTableSize();
 char KeepTesting();
-
+bool DuplicateValue(int [] , int , int );
+void InitializeRandomArray(int []);
 
 #endif /* defined(__townsendpeters_prog__OtherFunctions__) */
