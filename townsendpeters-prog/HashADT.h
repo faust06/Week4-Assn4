1db263f3b25026ff5f9d509fbceb95412e769b03
Chad <cpeters008@gmail.com>
2015-03-27 17:48:08 +0100

    Included your functions and QuadraticProbe / DoubleHashValue
    --I have not tested either function yet to see if they work.. I have to go to work now, but I'll test them there

--- townsendpeters-prog/HashADT.h
+++ townsendpeters-prog/HashADT.h
@@ -6,8 +6,14 @@
 
 #include "Common.h"
 
+int HashValue(int , int );
 double CalculateLoadFactor(int );
-void InitializeOpenTbl(int* & ,int* & , int );
+void InitializeOpenTbl(int* & ,int* & , int);
 void InitializeChnTbl(struct chnArray* , int );
+int FindOpenValue(int ,int [],int * ,int * , int );
+int FindChainValue(int [], struct chnArray *, int );
+void HashSearching(int, int [], int * , struct chnArray * , int , int *, double & , double & );
+int QuadraticProbe(int [] , int , int );
+int DoubleHashValue(int [], int , int , int );
 
 #endif /* defined(__townsendpeters_prog__HashADT__) */
